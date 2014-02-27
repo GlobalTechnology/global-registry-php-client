ad51cfc2dd60f94c043ec6d812ea1e095dcdb7b2
Brian Zoetewey <brian.zoetewey@ccci.org>
2014-02-27 16:53:09 -0500

    Improved entity test

--- tests/GlobalRepository/Model/EntityTest.php
+++ tests/GlobalRepository/Model/EntityTest.php
@@ -4,25 +4,39 @@
 
 	class EntityTest extends \PHPUnit_Framework_TestCase {
 
-		/**
-		 * @param $type
-		 * @param $data
-		 *
-		 * @dataProvider provideEntityParams
-		 */
-		public function testEntity( $type, $data ) {
+		public function testEntityFromArray() {
+			$type = 'entity';
+			$data = array(
+				'id'      => 1234567,
+				'attr1'   => 'One',
+				'attr2'   => 2,
+				'subAttr' => array(
+					'id'         => 1234568,
+					'subAttr1'   => 'One',
+					'subAttr2'   => 'Two',
+					'subSubAttr' => array(
+						'id'          => 1234569,
+						'subSubAttr1' => 'One',
+					),
+				),
+			);
+
 			$entity = new Entity( $type, $data );
 
+			$this->assertInstanceOf( '\GlobalTechnology\GlobalRegistry\Model\Entity', $entity );
 			$this->assertEquals( $type, $entity->type );
-			foreach ( $data as $name => $value ) {
-				$this->assertEquals( $value, $entity->$name );
-			}
-		}
-
-		public function provideEntityParams() {
-			return array(
-				array( 'type', array( array( 'name' => 'value' ) ) ),
-			);
+			$this->assertEquals( 1234567, $entity->id );
+			$this->assertEquals( 'One', $entity->attr1 );
+			$this->assertEquals( 2, $entity->attr2 );
+			$this->assertInstanceOf( '\GlobalTechnology\GlobalRegistry\Model\Entity', $entity->subAttr );
+			$this->assertEquals( 'subAttr', $entity->subAttr->type );
+			$this->assertEquals( 1234568, $entity->subAttr->id );
+			$this->assertEquals( 'One', $entity->subAttr->subAttr1 );
+			$this->assertEquals( 'Two', $entity->subAttr->subAttr2 );
+			$this->assertInstanceOf( '\GlobalTechnology\GlobalRegistry\Model\Entity', $entity->subAttr->subSubAttr );
+			$this->assertEquals( 'subSubAttr', $entity->subAttr->subSubAttr->type );
+			$this->assertEquals( 1234569, $entity->subAttr->subSubAttr->id );
+			$this->assertEquals( 'One', $entity->subAttr->subSubAttr->subSubAttr1 );
 		}
 	}
 }
