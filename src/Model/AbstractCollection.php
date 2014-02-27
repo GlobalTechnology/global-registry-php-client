96e93c160115fc9852653b3afb3b201d26ee3af0
Brian Zoetewey <brian.zoetewey@ccci.org>
2014-02-27 15:24:38 -0500

    Fixed multiple AbstractCollection bugs and removed ArrayAccess interface

--- src/Model/AbstractCollection.php
+++ src/Model/AbstractCollection.php
@@ -2,41 +2,23 @@
 	use Guzzle\Service\Command\ResponseClassInterface;
 	use Guzzle\Service\Command\OperationCommand;
 
-	abstract class AbstractCollection implements ResponseClassInterface, \IteratorAggregate, \ArrayAccess, \Countable {
+	abstract class AbstractCollection implements ResponseClassInterface, \IteratorAggregate, \Countable {
 
 		protected $collection = array();
 
 		abstract public static function fromJSON( $json = null );
 
 		public static function fromCommand( OperationCommand $command ) {
-			self::fromJSON( $command->getResponse()->json() );
+			$class = get_called_class();
+			return $class::fromJSON( $command->getResponse()->json() );
 		}
 
 		public function getIterator() {
-			return new \ArrayIterator( $this->items );
+			return new \ArrayIterator( $this->collection );
 		}
 
 		protected function __construct( $collection = array() ) {
 			$this->collection = $collection;
-		}
-
-		public function offsetSet( $offset, $value ) {
-			if ( is_null( $offset ) )
-				$this->collection[ ] = $value;
-			else
-				$this->collection[ $offset ] = $value;
-		}
-
-		public function offsetExists( $offset ) {
-			return isset( $this->collection[ $offset ] );
-		}
-
-		public function offsetUnset( $offset ) {
-			unset( $this->collection[ $offset ] );
-		}
-
-		public function offsetGet( $offset ) {
-			return isset( $this->collection[ $offset ] ) ? $this->collection[ $offset ] : null;
 		}
 
 		public function count() {
