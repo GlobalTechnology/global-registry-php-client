a22a1f507cbdfd1294d4ba9ac5bd9e41d98d22a1
Daniel Frett <daniel.frett@ccci.org>
2014-05-30 16:49:57 -0400

    give AbstractCollections ArrayAccess

--- src/Model/AbstractCollection.php
+++ src/Model/AbstractCollection.php
@@ -1,8 +1,8 @@
 <?php namespace GlobalTechnology\GlobalRegistry\Model {
-	use Guzzle\Service\Command\ResponseClassInterface;
 	use Guzzle\Service\Command\OperationCommand;
+	use Guzzle\Service\Command\ResponseClassInterface;
 
-	abstract class AbstractCollection implements ResponseClassInterface, \IteratorAggregate, \Countable {
+	abstract class AbstractCollection implements ResponseClassInterface, \IteratorAggregate, \ArrayAccess, \Countable {
 		const JSON_META         = 'meta';
 
 		protected $data;
@@ -47,6 +47,22 @@
 
 		public function count() {
 			return count( $this->data );
+		}
+
+		public function offsetExists( $offset ) {
+			return isset( $this->data[ $offset ] );
+		}
+
+		public function offsetGet( $offset ) {
+			return isset( $this->data[ $offset ] ) ? $this->data[ $offset ] : null;
+		}
+
+		public function offsetSet( $offset, $value ) {
+			$this->data[ $offset ] = $value;
+		}
+
+		public function offsetUnset( $offset ) {
+			unset( $this->data[ $offset ] );
 		}
 	}
 }
